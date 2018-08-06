0ca0ac55f851418dd4b0c65e5a0accd0f789f799
Pinchas Hodadad <pinchas.hodadad@gmail.com>
2018-08-06 16:11:36 +0300

    rails new

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_06_093708) do
+ActiveRecord::Schema.define(version: 2018_08_06_130256) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
